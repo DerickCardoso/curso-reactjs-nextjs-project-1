eadfe92ed269ced38a6f810f8d0955e0c7530f2f
DerickCSilva <derick.cardoso17@gmail.com>
2021-11-17 10:08:18 -0300

    feat: adicionando mais testes

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -10,60 +10,60 @@ import { Posts } from "../../components/Posts";
 import { loadPosts } from "../../utils/load-post";
 
 export const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(8);
-  const [searchValue, setSearchValue] = useState("");
+    const [posts, setPosts] = useState([]);
+    const [allPosts, setAllPosts] = useState([]);
+    const [page, setPage] = useState(0);
+    const [postsPerPage] = useState(2);
+    const [searchValue, setSearchValue] = useState("");
 
-  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
-    const postsAndPhotos = await loadPosts();
+    const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+        const postsAndPhotos = await loadPosts();
 
-    setPosts(postsAndPhotos.slice(page, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  }, []);
+        setPosts(postsAndPhotos.slice(page, postsPerPage));
+        setAllPosts(postsAndPhotos);
+    }, []);
 
-  useEffect(() => {
-    console.log(new Date().toLocaleString("pt-BR"));
-    handleLoadPosts(0, postsPerPage);
-  }, [handleLoadPosts, postsPerPage]);
+    useEffect(() => {
+        // console.log(new Date().toLocaleString("pt-BR"));
+        handleLoadPosts(0, postsPerPage);
+    }, [handleLoadPosts, postsPerPage]);
 
-  const loadMorePosts = () => {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    posts.push(...nextPosts);
+    const loadMorePosts = () => {
+        const nextPage = page + postsPerPage;
+        const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+        posts.push(...nextPosts);
 
-    setPosts(posts);
-    setPage(nextPage);
-  };
+        setPosts(posts);
+        setPage(nextPage);
+    };
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
-  };
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setSearchValue(value);
+    };
 
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-  const filteredPosts = searchValue
-    ? allPosts.filter((post) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-    : posts;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+    const filteredPosts = searchValue
+        ? allPosts.filter((post) => {
+              return post.title.toLowerCase().includes(searchValue.toLowerCase());
+          })
+        : posts;
 
-  return (
-    <section className="container">
-      <div className="search-container">
-        {!!searchValue && <h1>Search value: {searchValue}</h1>}
-        <TextInput searchValue={searchValue} handleChange={handleChange} />
-      </div>
+    return (
+        <section className="container">
+            <div className="search-container">
+                {!!searchValue && <h1>Search value: {searchValue}</h1>}
+                <TextInput searchValue={searchValue} handleChange={handleChange} />
+            </div>
 
-      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+            {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
 
-      {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
-      <div className="button-container">
-        {!searchValue && <Button text="Load more posts" onClick={loadMorePosts} disabled={noMorePosts} />}
-      </div>
-    </section>
-  );
+            {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
+            <div className="button-container">
+                {!searchValue && <Button text="Load more posts" onClick={loadMorePosts} disabled={noMorePosts} />}
+            </div>
+        </section>
+    );
 };
 
 export default Home;
